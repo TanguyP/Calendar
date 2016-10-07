139af822d9b71f06e477892732f2f096cf98bebd
Tanguy P <tanguy.penet@gmail.com>
2016-10-07 22:27:19 +0200

    Settings: update INSTALLED_APPS

--- apiproxy/apiproxy/settings.py
+++ apiproxy/apiproxy/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'rest_framework.authtoken',
+	
+	# Project's apps
+	'events',
 )
 
 MIDDLEWARE_CLASSES = (
